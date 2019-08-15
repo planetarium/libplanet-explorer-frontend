58555bd5e01244e1ee3e1a9f31b667224c7b713e
lime <lime_bell@naver.com>
2019-08-15 17:25:20 +0900

    Fix rendering issue
    https://github.com/planetarium/libplanet-explorer-frontend/issues/14
    
    Co-authored-by: JongChan Choi <jong@chan.moe>

--- src/misc/useQueryString.ts
+++ src/misc/useQueryString.ts
@@ -1,11 +1,11 @@
-import { useState, useEffect } from 'react';
+import { useCallback, useMemo } from 'react';
 import { navigate, withPrefix } from 'gatsby';
 
 export default function useQueryString(location: Location) {
-  const [queryString, setQueryString] = useState(location.search.substr(1));
-  useEffect(() => {
+  const queryString = useMemo(() => location.search.substr(1), [location]);
+  const setQueryString = useCallback((queryString: string) => {
     const path = location.pathname.substr(withPrefix('/').length - 1);
     navigate(path + (queryString ? `?${queryString}` : ''));
-  }, [queryString]);
+  }, [location, navigate]);
   return [queryString, setQueryString] as const;
 }
