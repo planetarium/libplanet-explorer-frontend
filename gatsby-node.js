a9df134b416bd749105cd443a31a787f8d07efc6
Hyunwoo Park <ez.amiryo@gmail.com>
2020-01-14 21:17:03 +0900

    Read subpages by fs.readdir (#83)
    Co-authored-by: Hong Minhee <hong.minhee@gmail.com>

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,21 +1,33 @@
+const fs = require('fs');
 const path = require('path');
 const GRAPHQL_ENDPOINTS = JSON.parse(process.env.GRAPHQL_ENDPOINTS);
 
-exports.createPages = ({ graphql, actions }) => {
+function readdirAsync(path) {
+  return new Promise(function (resolve, reject) {
+    fs.readdir(path, function (error, result) {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(result);
+      }
+    });
+  });
+}
+
+exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions;
-  const files = [
-    { name: 'list.tsx', path: '' },
-    { name: 'account.tsx', path: 'account' },
-    { name: 'block.tsx', path: 'block' },
-    { name: 'search.tsx', path: 'search' },
-    { name: 'transaction.tsx', path: 'transaction' },
-  ];
+  const files = await readdirAsync('src/subpages');
+  const remap = {
+    'list.tsx': '',
+  };
 
   GRAPHQL_ENDPOINTS.forEach(endpoint => {
     files.forEach(file => {
+      const generatedPath = file in remap ? remap[file] : path.parse(file).name;
+
       createPage({
-        path: `${endpoint.name}/${file.path}`,
-        component: path.resolve(`src/subpages/${file.name}`),
+        path: `${endpoint.name}/${generatedPath}`,
+        component: path.resolve(`src/subpages/${file}`),
         isPermanent: true,
         context: {
           endpoint,
