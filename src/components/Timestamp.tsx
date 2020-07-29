845038e44175d3022ee0e831ccf8d6d776d27b84
Muhun Kim <iam@muhun.kim>
2020-07-29 20:27:00 +0900

    Replace Intl format options for Browser compatibility
    Reference issue: #92
    
    dateStyle and timeStyle options not yet implemented in some browsers.
    
    I replace options to output same result.

--- src/components/Timestamp.tsx
+++ src/components/Timestamp.tsx
@@ -6,9 +6,12 @@ interface TimestampProps {
 }
 const Timestamp: React.FC<TimestampProps> = ({ timestamp }) => {
   const date = new Date(timestamp);
-  const formatOptions: any = {
-    dateStyle: 'short',
-    timeStyle: 'short',
+  const formatOptions = {
+    hour: '2-digit',
+    minute: '2-digit',
+    year: '2-digit',
+    month: 'numeric',
+    day: 'numeric',
   };
   const now = new Date();
   let string = date.toLocaleString(undefined, formatOptions);
@@ -17,7 +20,8 @@ const Timestamp: React.FC<TimestampProps> = ({ timestamp }) => {
     now.getMonth() == date.getMonth() &&
     now.getDate() == date.getDate()
   ) {
-    string = date.toLocaleTimeString(undefined, formatOptions);
+    const { year, month, day, ...pickFormatOptions } = formatOptions;
+    string = date.toLocaleString(undefined, pickFormatOptions);
   }
   return (
     <time dateTime={date.toISOString()} title={date.toLocaleString()}>
