245e5ceffa417ba1b91206f5168de7f2da998b10
moreal <dev.moreal@gmail.com>
2020-10-29 19:39:37 +0900

    feat: apply action type restruction
    It resolves break changes in
    https://github.com/planetarium/libplanet-explorer/pull/131

--- src/subpages/transaction.tsx
+++ src/subpages/transaction.tsx
@@ -3,10 +3,48 @@ import useQueryString from '../misc/useQueryString';
 import { TransactionByIdComponent } from '../generated/graphql';
 import { Link } from '@fluentui/react';
 import Timestamp from '../components/Timestamp';
+import { decode, BencodexValue } from "bencodex";
+import JSONTree from "react-json-tree";
 
 interface TransactionPageProps {
   location: Location;
 }
+
+// FIXME: do not use any type.
+function convertToObject(value: BencodexValue | undefined): any {
+  if (value instanceof Map) {
+    return Object.fromEntries(Array.from(value).map(v => [v[0], convertToObject(v[1])]));
+  } else if (value instanceof Array) {
+    return value.map(v => convertToObject(v));
+  } else if (value instanceof Uint8Array) {
+    return "<binary> " + value.toString('hex');
+  } else {
+    return value;
+  }
+}
+
+// Used apathy theme and modified only base00 color.
+// https://github.com/reduxjs/redux-devtools/blob/75322b15ee7ba03fddf10ac3399881e302848874/src/react/themes/apathy.js
+const jsonTreeTheme = {
+  scheme: 'apathy',
+  author: 'jannik siebert (https://github.com/janniks)',
+  base00: '#faf9f8;',
+  base01: '#0B342D',
+  base02: '#184E45',
+  base03: '#2B685E',
+  base04: '#5F9C92',
+  base05: '#81B5AC',
+  base06: '#A7CEC8',
+  base07: '#D2E7E4',
+  base08: '#3E9688',
+  base09: '#3E7996',
+  base0A: '#3E4C96',
+  base0B: '#883E96',
+  base0C: '#963E4C',
+  base0D: '#96883E',
+  base0E: '#4C963E',
+  base0F: '#3E965B'
+};
 
 const TransactionPage: React.FC<TransactionPageProps> = ({ location }) => {
   const [queryString] = useQueryString(location);
@@ -52,6 +90,20 @@ const TransactionPage: React.FC<TransactionPageProps> = ({ location }) => {
                 </Link>
               </dd>
             ));
+
+        const actions =
+          transaction.actions.map((action, index) => (
+            <dd key={index}>
+              <dl>
+                <JSONTree
+                  data={convertToObject(decode(Buffer.from(action.raw, 'base64')))}
+                  theme={jsonTreeTheme}
+                  invertTheme={false}
+                  hideRoot={true}/>
+              </dl>
+            </dd>
+          ));
+
         // FIXME: We'd better to use absolute paths and make Gatsby to
         // automatically rebase these absolute paths on the PATH_PREFIX
         // configuration.
@@ -100,25 +152,7 @@ const TransactionPage: React.FC<TransactionPageProps> = ({ location }) => {
               <dt>Block Reference</dt>
               {blockRef}
               <dt>Actions</dt>
-              {transaction.actions.map((action, index) => (
-                <dd key={index}>
-                  <dl>
-                    {action.arguments.map(argument => (
-                      <React.Fragment key={argument.key}>
-                        <dt>{argument.key}</dt>
-                        <dd>
-                          <pre>
-                            <code>
-                              {' '}
-                              {JSON.stringify(argument.value, null, 2)}{' '}
-                            </code>
-                          </pre>
-                        </dd>
-                      </React.Fragment>
-                    ))}
-                  </dl>
-                </dd>
-              ))}
+              {actions}
             </dl>
           </>
         );
