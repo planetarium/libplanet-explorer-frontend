bd795988ea9ababab3878211d59450f841858dc5
Seo Myunggyun (Jonathan) <tkiapril@users.noreply.github.com>
2023-02-14 00:25:44 +0900

    temp

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,10 +1,20 @@
-import { ReactNode, useEffect, useState } from 'react';
+import { ReactNode, useEffect, useMemo, useRef, useState } from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import { Encodable, decode, encode } from 'bencodex';
 import { ApolloProvider, useApolloClient } from '@apollo/client/react';
-import { ApolloClient, ApolloError, InMemoryCache } from '@apollo/client';
+import {
+  ApolloClient,
+  ApolloError,
+  ApolloQueryResult,
+  InMemoryCache,
+} from '@apollo/client';
 import styled from '@emotion/styled';
 import {
+  DefaultButton,
+  Dialog,
+  DialogFooter,
+  DialogType,
   DirectionalHint,
   Dropdown,
   IDropdownStyles,
@@ -12,10 +22,19 @@ import {
   IRenderFunction,
   Icon,
   NeutralColors,
+  Panel,
+  PrimaryButton,
   SearchBox,
+  TextField,
   TooltipDelay,
   TooltipHost,
+  ITextField,
+  Pivot,
+  PivotItem,
 } from '@fluentui/react';
+import { createAccount, isValidPrivateKey } from '@planetarium/account-raw';
+import { Account, deriveAddress, signTransaction } from '@planetarium/sign';
+import { encodeTxMetadata } from '@planetarium/tx';
 
 import { GraphQLEndPoint, GRAPHQL_ENDPOINTS } from 'lib/graphQLEndPoint';
 import {
@@ -23,9 +42,18 @@ import {
   BlockByIndexDocument,
   BlockByHashQuery,
   BlockByHashDocument,
+  Get9cTransferAssetActionQuery,
+  Get9cTransferAssetActionDocument,
   TransactionByIdQuery,
   TransactionByIdDocument,
+  CurrencyEnum,
+  StageTransaction9cMutation,
+  StageTransaction9cDocument,
 } from 'src/gql/graphql';
+import {
+  NextAccountNonceQuery,
+  NextAccountNonceDocument,
+} from 'src/explorerGql/graphql';
 
 import Wrapper from 'components/Wrapper';
 import { CommonPageProps } from 'lib/staticGeneration';
@@ -97,6 +125,8 @@ export function NavBar({ endpoint }: { endpoint: GraphQLEndPoint }) {
   const { push } = useRouter();
   const [searchBoxValue, setSearchBoxValue] = useState('');
   const itemId = useQueryItemId();
+  const [keyMap, setKeyMap] = useState(new Map<string, string>());
+  const [signPanelOpen, setSignPanelOpen] = useState(false);
   useEffect(() => {
     setSearchBoxValue(itemId ?? '');
   }, [itemId]);
@@ -229,8 +259,313 @@ export function NavBar({ endpoint }: { endpoint: GraphQLEndPoint }) {
             styles={dropdownStyles}
           />
         </NetworkNameContainer>
+        <Icon
+          iconName={
+            keyMap.get(endpoint.uri) === undefined
+              ? 'AzureKeyVault'
+              : 'InsertSignatureLine'
+          }
+          style={{ color: 'white', fontSize: '1.5rem', margin: '0.5rem' }}
+          onClick={() => setSignPanelOpen(true)}
+        />
+        <Panel
+          isOpen={signPanelOpen}
+          onDismiss={() => setSignPanelOpen(false)}
+          isHiddenOnDismiss={true}
+          isBlocking={false}
+        >
+          <KeyPanel
+            endpoint={endpoint}
+            nineChroniclesApolloClient={client}
+            keyMap={keyMap}
+            setKeyMap={setKeyMap}
+          />
+        </Panel>
       </NavWrapper>
     </StyledNav>
+  );
+}
+
+function KeyPanel({
+  endpoint,
+  nineChroniclesApolloClient,
+  keyMap,
+  setKeyMap,
+}: {
+  endpoint: GraphQLEndPoint;
+  nineChroniclesApolloClient: ApolloClient<object> /* FIXME: swap with explorer client */;
+  keyMap: Map<string, string>;
+  setKeyMap: (
+    value:
+      | Map<string, string>
+      | ((prev: Map<string, string>) => Map<string, string>)
+  ) => void;
+}) {
+  const keyFieldRef = useRef<ITextField>(null);
+  const recipientFieldRef = useRef<ITextField>(null);
+  const amountFieldRef = useRef<ITextField>(null);
+  const memoFieldRef = useRef<ITextField>(null);
+  const [selectedCurrency, setSelectedCurrency] = useState<string>(
+    Object.values(CurrencyEnum)[0]
+  );
+  const [transferConfirmationDialogOpen, setTransferConfirmationDialogOpen] =
+    useState(false);
+  const explorerApolloClient = useMemo(
+    () =>
+      new ApolloClient({
+        cache: new InMemoryCache(),
+        uri: endpoint.uri + '/explorer',
+      }),
+    [endpoint]
+  ); /* FIXME: swap with 9c client */
+  const key = useMemo(() => keyMap.get(endpoint.uri), [keyMap, endpoint]);
+  const account = useMemo(() => (key ? createAccount(key) : undefined), [key]);
+  const [signer, setSigner] = useState<string>();
+  const [signerBuffer, setSignerBuffer] = useState<Buffer>();
+  const { push } = useRouter();
+  useEffect(() => {
+    async function getSigner() {
+      const signer = account ? await deriveAddress(account) : undefined;
+      setSigner(signer);
+      setSignerBuffer(
+        signer ? Buffer.from(signer!.slice(2), 'hex') : undefined
+      );
+    }
+    getSigner();
+  }, [account]);
+  const [genesisHash, setGenesisHash] = useState<Buffer>();
+  async function getGenesisHash() {
+    const { data, error } = (await nineChroniclesApolloClient
+      .query<BlockByIndexQuery>({
+        query: BlockByIndexDocument,
+        variables: { index: 0 },
+      })
+      .catch(e => alert(e.message))) as ApolloQueryResult<BlockByIndexQuery>;
+    if (error) {
+      console.log(error);
+      return;
+    }
+    setGenesisHash(Buffer.from(data.chainQuery.blockQuery!.block!.hash, 'hex'));
+  }
+  useEffect(() => {
+    getGenesisHash();
+  }, [endpoint]);
+  if (key === undefined) {
+    return (
+      <>
+        <TextField componentRef={keyFieldRef} />
+        <DefaultButton
+          text="Load key"
+          onClick={() => {
+            try {
+              if (isValidPrivateKey(keyFieldRef.current?.value ?? '')) {
+                setKeyMap(keyMap =>
+                  new Map(keyMap).set(
+                    endpoint!.uri,
+                    keyFieldRef.current?.value ?? ''
+                  )
+                );
+              } else {
+                alert('Not a valid private key.');
+              }
+            } catch (e: any) {
+              alert(e.message);
+            }
+          }}
+        />
+      </>
+    );
+  }
+  return (
+    <>
+      Account{' '}
+      <b>
+        {signer?.slice(0, 10)} ... {signer?.slice(32)}
+      </b>
+      <Pivot>
+        <PivotItem headerText="Transfer">
+          <Pivot>
+            <PivotItem headerText="9c">
+              <TextField label="Recipient" componentRef={recipientFieldRef} />
+              <TextField
+                label="Amount"
+                componentRef={amountFieldRef}
+                style={{ textAlign: 'right' }}
+                onRenderSuffix={() => (
+                  <Dropdown
+                    options={Object.values(CurrencyEnum).map(item => ({
+                      key: item,
+                      text: item,
+                    }))}
+                    onChange={(_, option) =>
+                      setSelectedCurrency(
+                        (option as IDropdownOption).key as string
+                      )
+                    }
+                    selectedKey={selectedCurrency}
+                    styles={{
+                      title: {
+                        border: 0,
+                        backgroundColor: 'transparent',
+                      },
+                    }}
+                  />
+                )}
+              />
+              <TextField label="Memo" componentRef={memoFieldRef} />
+              <DefaultButton
+                text="Send"
+                onClick={() => setTransferConfirmationDialogOpen(true)}
+              />
+            </PivotItem>
+          </Pivot>
+          <Dialog
+            hidden={!transferConfirmationDialogOpen}
+            onDismiss={() => setTransferConfirmationDialogOpen(false)}
+            dialogContentProps={{
+              type: DialogType.normal,
+              title: 'Confirm Transfer',
+            }}
+          >
+            Transfer <b>{amountFieldRef.current?.value} NCG</b> to{' '}
+            <b>{recipientFieldRef.current?.value}</b> with{' '}
+            {memoFieldRef.current?.value ? (
+              <>
+                the memo{' '}
+                <span
+                  style={{
+                    backgroundColor: 'lightgray',
+                    color: 'indianred',
+                    padding: '0 0.25rem',
+                    borderRadius: '0.25rem',
+                  }}
+                >
+                  {memoFieldRef.current?.value}
+                </span>
+              </>
+            ) : (
+              <>
+                <b>no</b> memo
+              </>
+            )}
+            ?
+            <DialogFooter>
+              <DefaultButton
+                onClick={async () => {
+                  const { data, error } = (await nineChroniclesApolloClient
+                    .query<Get9cTransferAssetActionQuery>({
+                      query: Get9cTransferAssetActionDocument,
+                      variables: {
+                        sender: signer,
+                        recipient: recipientFieldRef.current?.value,
+                        amount: amountFieldRef.current?.value,
+                        currency: selectedCurrency,
+                        memo: memoFieldRef.current?.value,
+                      },
+                    })
+                    .catch(e =>
+                      alert(e.message)
+                    )) as ApolloQueryResult<Get9cTransferAssetActionQuery>;
+                  if (error) {
+                    console.log(error);
+                    setTransferConfirmationDialogOpen(false);
+                    alert(
+                      'Error retrieving 9c transferAsset action payload. Consult log for further information.'
+                    );
+                    return;
+                  }
+                  const { data: nonceData, error: nonceError } =
+                    (await explorerApolloClient
+                      .query<NextAccountNonceQuery>({
+                        query: NextAccountNonceDocument,
+                        variables: {
+                          address: signer,
+                        },
+                      })
+                      .catch(e =>
+                        alert(e.message)
+                      )) as ApolloQueryResult<NextAccountNonceQuery>;
+                  if (nonceError) {
+                    console.log(nonceError);
+                    setTransferConfirmationDialogOpen(false);
+                    alert(
+                      'Error retrieving nonce. Consult log for further information.'
+                    );
+                    return;
+                  }
+                  if (!genesisHash) {
+                    await getGenesisHash();
+                  }
+                  if (!genesisHash) {
+                    setTransferConfirmationDialogOpen(false);
+                    alert(
+                      'Error retrieving genesis hash. Consult log for further information.'
+                    );
+                    return;
+                  }
+
+                  const metadata: Encodable = encodeTxMetadata({
+                    nonce: BigInt(nonceData.transactionQuery?.nextNonce),
+                    publicKey: (await account!.getPublicKey()) as Uint8Array,
+                    signer: signerBuffer as Buffer,
+                    timestamp: new Date(),
+                    updatedAddresses:
+                      signer !== recipientFieldRef.current!.value
+                        ? new Set([
+                            signerBuffer as Buffer,
+                            Buffer.from(
+                              recipientFieldRef.current!.value!.startsWith('0x')
+                                ? recipientFieldRef.current!.value!.slice(2)
+                                : recipientFieldRef.current!.value!,
+                              'hex'
+                            ),
+                          ])
+                        : new Set([signerBuffer as Buffer]),
+                    genesisHash: genesisHash,
+                  });
+                  metadata.set(Buffer.from([0x61]), [
+                    decode(Buffer.from(data!.actionQuery.transferAsset, 'hex')),
+                  ]);
+
+                  const { data: stageData, error: stageError } =
+                    (await nineChroniclesApolloClient
+                      .mutate<StageTransaction9cMutation>({
+                        mutation: StageTransaction9cDocument,
+                        variables: {
+                          payload: await signTransaction(
+                            encode(metadata).toString('hex'),
+                            account as Account
+                          ),
+                        },
+                      })
+                      .catch(e =>
+                        alert(e.message)
+                      )) as ApolloQueryResult<StageTransaction9cMutation>;
+                  if (stageError) {
+                    console.log(stageError);
+                    setTransferConfirmationDialogOpen(false);
+                    alert(
+                      'Error staging transaction. Consult log for further information.'
+                    );
+                    return;
+                  }
+
+                  setTransferConfirmationDialogOpen(false);
+                  push(
+                    `/${endpoint.name}/transaction/?${stageData.stageTransaction}`
+                  );
+                }}
+                text="Send"
+              />
+              <PrimaryButton
+                onClick={() => setTransferConfirmationDialogOpen(false)}
+                text="Cancel"
+              />
+            </DialogFooter>
+          </Dialog>
+        </PivotItem>
+      </Pivot>
+    </>
   );
 }
 
